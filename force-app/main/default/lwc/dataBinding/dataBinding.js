0a35c39ab6093347d842f2e6dcb78a63422ca40d
Himadri-sfdc <paul.himadri.sfdc@gmail.com>
2025-06-24 14:43:23 +0530

    Data binding

---
+++ force-app/main/default/lwc/dataBinding/dataBinding.js
@@ -0,0 +1,12 @@
+import { LightningElement } from 'lwc';
+
+export default class DataBinding extends LightningElement {
+
+    employee = {
+
+        fname : 'Himadri',
+        lname : 'Paul',
+        position : 'Salesforce Developer',
+        age : 32
+    }
+}
