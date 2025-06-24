c765c70588b2bdd71ac91ae506158fcf2dfff859
Himadri-sfdc <paul.himadri.sfdc@gmail.com>
2025-06-24 16:00:50 +0530

    Data Binding With getter Method

--- force-app/main/default/lwc/dataBinding/dataBinding.js
+++ force-app/main/default/lwc/dataBinding/dataBinding.js
@@ -7,6 +7,12 @@ export default class DataBinding extends LightningElement {
         fname : 'Himadri',
         lname : 'Paul',
         position : 'Salesforce Developer',
-        age : 32
+        exp : 10
+    }
+
+    get EmployeeValue(){
+        
+        const status = this.employee.exp <=9?'Broze': this.employee.exp >=10 ?'silver':this.employee.exp >=20?'Gold':'no status';
+        return status;
     }
 }
