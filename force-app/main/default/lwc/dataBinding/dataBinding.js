f59024a154cab0a9f665b7e81b1eb24253ab5c73
Himadri-sfdc <paul.himadri.sfdc@gmail.com>
2025-06-27 03:34:59 +0530

    Data binding one -way two-way

--- force-app/main/default/lwc/dataBinding/dataBinding.js
+++ force-app/main/default/lwc/dataBinding/dataBinding.js
@@ -15,4 +15,10 @@ export default class DataBinding extends LightningElement {
         const status = this.employee.exp <=9?'Broze': this.employee.exp >=10 ?'silver':this.employee.exp >=20?'Gold':'no status';
         return status;
     }
+
+    FullName = 'Himadri paul';
+
+    nameChangeHandler(event){
+        this.FullName = event.target.value;
+    }
 }
